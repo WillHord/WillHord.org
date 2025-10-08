ca04de7ff3139d07bb736f63aa720474f5430590
Will <willhord@gmail.com>
2025-10-08 12:14:49 -0400

    fix: move font import to top of css and fix space in tech skills

--- src/lib/components/mobileNav.svelte
+++ src/lib/components/mobileNav.svelte
@@ -13,7 +13,7 @@ let { pathname } = $props();
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    className="h-5 w-5"
+    class="h-5 w-5"
   >
     <path
       d="M3 5H11"
